b22e012b57ad0b857e64fe15f50ad1c98672e40a
Ani Davtyan <missanidavtyan@gmail.com>
2021-06-04 09:58:16 +0200

    Final commit

--- scripts/meny.js
+++ scripts/meny.js
@@ -1,11 +1,20 @@
+//Creates input elements and saves it in variable search
 var search = document.createElement("input");
+//Adds id attribute to the search variable
 search.setAttribute('id', 'searchVal');
+//Creates jquery collection that involves all h4 elements
 var h = $("h4");
+//Create and empathy array calles arrayH
 var arrayH = [];
+//Creates ul element and saves it in ulEl variable
 var ulEl = $("<ul/>");
+//Defines Liel global variable
 var liEL
+//Create 'cache' array
 var cache = [];
+//Defines the star value
 var starValue
+//localStorage.clear();
 
 $(document).ready(generateMenu);
 
@@ -29,32 +38,70 @@ function generateMenu() {
         $(h[i]).attr('id', i);
         //Man kan ochså använda settAttribute
         //h[i].setAttribute('id',i);
-        var stars = '<div class = "stars"><a id = "1">⭐</a><a id = "2">⭐</a><a id = "3">⭐</a><a id = "4">⭐</a><a id = "5">⭐</a></div>';
-        ulEl.append("<li><a href = 'index.html#" + i + "'>" + h[i].textContent + "</a>" + stars + '</li>');
-        /*
-        document.getElementsAll('.stars a').Each(function(a){
-            a.addEventListener('click', 
-            console.log(indexOf(a)))
-        })
-        */
+        var text = h[i].textContent;
+        //console.log(text.replace(/\s/g, "").replace(/,/g, ''));
+        var stars = '<div class = "stars" id ='+text.replace(/\s/g, "").replace(/,/g, '')+'><a id = "1">⭐</a><a id = "2">⭐</a><a id = "3">⭐</a><a id = "4">⭐</a><a id = "5">⭐</a></div>';
+        //console.log($('.stars'));
+        ulEl.append("<li><a href = 'index.html#" + i + "'>" + text + "</a>" + stars + '</li>');
+        //Check if rating is used for all elements
+        //console.log($('#' + text));
+        
+        if(!(localStorage.getItem(text))){
+            console.log(localStorage.getItem(text));
+            console.log($('#' + text.replace(/\s/g, "")));
+            $('#' + text.replace(/\s/g, "").replace(/,/g, '') + ' a').css({'opacity':'50%', 'cursor': 'pointer'});
+        }
+        
+        else {
+            //console.log(text);
+            
+           //code doesn't work
+           ($('#' + text.replace(/\s/g, "").replace(/,/g, '') + ' #' + localStorage.getItem(text)))
+           .nextAll().css({'opacity': '50%', 'cursor': 'pointer'});
+
+           /*
+           ($('#' + text.replace(/\s/g, "") + '#' + localStorage.getItem(text))).nextAll().animate({'opacity': '50%'});
+           ($('#' + text.replace(/\s/g, "") + '#' + localStorage.getItem(text))).animate({'opacity': '100%'}, 'slow');
+           ($('#' + text.replace(/\s/g, "") + '#' + localStorage.getItem(text))).prevAll().animate({'opacity': '100%'}, 'slow');
+            //$('#' + text.replace(/\s/g, "")).css({'opacity':'50%', 'cursor': 'pointer', 'font-size': '1em'});
+            */
+        }
+        
+        
+        //console.log(localStorage.getItem(text));
+        $('a:not([id])');
+
 
     };    
 
     
-    $('.stars a').css({'opacity':'50%', 'cursor': 'pointer', 'font-size': '0.8em'});
+    
     $('.stars a').on("click",function(){
-        //$(this).animate({'opacity': '50%'})
+        console.log($(this.parentNode));
+        console.log($(this));
+        //$(this.parentNode).animate({'opacity':'100%', 'cursor': 'pointer'}, 'fast');
+        
+        //$('.stars a').css({'opacity':'100%', 'cursor': 'pointer', 'font-size': '0.8em'});
         $(this).nextAll().animate({'opacity': '50%'})
+        //Does not reset the value
         $(this).animate({'opacity': '100%'}, 'slow')
+        //Does not reset the value
         $(this).prevAll().animate({'opacity': '100%'}, 'slow')
+        //$(this.parentNode).css({'opacity':'unset', 'cursor': 'pointer'});
         /*
         if (window.localStorage){
             localStorage.setItem()
         }
         */
+        var menyItem = this.parentNode.previousSibling.textContent
+        //console.log(menyItem);
+        //star is a global variable
         starValue = this.id;
-        console.log(this.parentNode.previousSibling.textContent);
-        console.log(document.querySelectorAll(".stars a"));
+        localStorage.setItem(menyItem, starValue);
+        //console.log(localStorage.getItem('Potatismos'));
+        //console.log(starValue);
+        //console.log(this.parentNode.previousSibling.textContent);
+        //console.log(document.querySelectorAll(".stars a"));
 
     });
   
@@ -64,7 +111,7 @@ console.log($("#receptmeny ul li"));
 */
 var inputVal = $("#searchVal");
     
-console.log($("#receptmeny ul li"));
+//console.log($("#receptmeny ul li"));
 $("#receptmeny ul li").each(function(){
     cache.push({
         element: this,
@@ -77,7 +124,7 @@ $("#receptmeny ul li").each(function(){
 }
 
 (function(){
-    console.log(cache);
+    //console.log(cache);
     function filter(){
         var query = this.value.trim().toLowerCase();
         cache.forEach(function(li){
@@ -102,41 +149,10 @@ $("#receptmeny ul li").each(function(){
 }());
 
 /*
-
-(function(){
-    var inputVal = $("#searchVal");
-    var cache = [];
-    $('div .post').each(function(){
-        cache.push({
-            element: this,
-            text: this.firstElementChild.innerHTML.trim().toLowerCase()
-        });
-    });
-
-    
-    function filter(){
-        var query = this.value.trim().toLowerCase();
-        cache.forEach(function(div){
-            var index = 0;
-            if (query) {
-                index = div.text.indexOf(query);
-            }
-
-            div.element.style.display = index === -1 ? 'none' : '';
-        });  
-    }
-    
-    if ('oninput' in search){
-        $(search).on('input', filter);
-        
-    } else {
-        $(search).on('keyup', filter);
-        
-    }
-
-    
-    
-}());
+ids  for the list strar elemet are not correct, only the first words is included in the id value
+check if the element can be choosen in jquery when a variable is used as a selector id - done
+check if value of a element can be chosen as a jquery seletor - no nessesry
+short out how stars should look when the rating is give to the stars
 
 */
 
